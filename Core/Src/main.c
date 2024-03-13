01881917e4439d4111c50942f0adfeead4515b30
Nathan Ray <71066202+nathan-ray@users.noreply.github.com>
2024-03-13 10:27:49 -0600

    added LED debuggers

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,6 +1,6 @@
-/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
+	* @name						: Nathaniel Ray Raharjo | u1360092
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
@@ -15,46 +15,15 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
 /* Private variables ---------------------------------------------------------*/
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
@@ -62,40 +31,245 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	__HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
+	
+	// ORANGE -> 9
+	// GREEN  -> 8
+	// BLUE		-> 7
+	// RED		-> 6
+	// Set up a configuration struct to pass to the initialization function
+	GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_6 | GPIO_PIN_7,
+	GPIO_MODE_OUTPUT_PP,
+	GPIO_SPEED_FREQ_LOW,
+	GPIO_NOPULL};
+	HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC6, PC7, PC8 & PC9
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET); // Start PC9 high
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET); // Start PC8 high
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET); // Start PC7 high
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET); // Start PC6 high
+	
+	// RCC enable for GPIOB and GPIOC
+	RCC->AHBENR |= (1 << 18); // B
+	RCC->AHBENR |= (1 << 19); // C
+	
+	// RCC enable for I2C2
+	RCC->APB1ENR |= (1 << 22);
+	
+	// GPIO PB11 to AF
+	GPIOB->MODER |= (1 << 23);
+	GPIOB->MODER &= ~(1 << 22);
+	
+	// GPIO PB11 to open drain
+	GPIOB->OTYPER |= (1 << 11);
+	
+	// GPIO PB11 to I2C2_SDA AF1 (0001)
+	GPIOB->AFR[1] &= ~(1 << 15);
+	GPIOB->AFR[1] &= ~(1 << 14);
+	GPIOB->AFR[1] &= ~(1 << 13);
+	GPIOB->AFR[1] |= (1 << 12);
+	
+	// GPIO PB13 to AF
+	GPIOB->MODER |= (1 << 27);
+	GPIOB->MODER &= ~(1 << 26);
+	
+	// GPIO PB13 to open drain
+	GPIOB->OTYPER |= (1 << 13);
+	
+	// GPIO PB13 to I2C2_SCL AF5 (0101)
+	GPIOB->AFR[1] &= ~(1 << 23);
+	GPIOB->AFR[1] |= (1 << 22);
+	GPIOB->AFR[1] &= ~(1 << 21);
+	GPIOB->AFR[1] |= (1 << 20);
+	
+	// GPIO PB14 to output mode
+	GPIOB->MODER &= ~(1 << 29);
+	GPIOB->MODER |= (1 << 28);
+	
+	// GPIO PB14 push pull
+	GPIOB->OTYPER &= ~(1 << 14);
+	
+	// GPIO PB14 init high
+	GPIOB->ODR |= (1 << 14);
+	
+	// GPIO PC0 to output mode
+	GPIOC->MODER &= ~(1 << 1);
+	GPIOC->MODER |= (1 << 0);
+	
+	// GPIO PC0 to push pull
+	GPIOC->OTYPER &= ~(1 << 0);
+	
+	// GPIO PC0 init high
+	GPIOC->ODR |= (1 << 0);
+	
+	// TIMINGR to 100 kHz
+	// PRESC to 1
+	I2C2->TIMINGR &= ~(1 << 31);
+	I2C2->TIMINGR &= ~(1 << 30);
+	I2C2->TIMINGR &= ~(1 << 29);
+	I2C2->TIMINGR |= (1 << 28);
+	// SCLDEL to 0x4 or 0'b0100
+	I2C2->TIMINGR &= ~(1 << 23);
+	I2C2->TIMINGR |= (1 << 22);
+	I2C2->TIMINGR &= ~(1 << 21);
+	I2C2->TIMINGR &= ~(1 << 20);
+	// SDADEL to 0x2 or 0'b0010
+	I2C2->TIMINGR &= ~(1 << 19);
+	I2C2->TIMINGR &= ~(1 << 18);
+	I2C2->TIMINGR |= (1 << 17);
+	I2C2->TIMINGR &= ~(1 << 16);
+	// SCLH to 0x0F or 0'b1111
+	I2C2->TIMINGR &= ~(1 << 15);
+	I2C2->TIMINGR &= ~(1 << 14);
+	I2C2->TIMINGR &= ~(1 << 13);
+	I2C2->TIMINGR &= ~(1 << 12);
+	I2C2->TIMINGR |= (1 << 11);
+	I2C2->TIMINGR |= (1 << 10);
+	I2C2->TIMINGR |= (1 << 9);
+	I2C2->TIMINGR |= (1 << 8);
+	// SCLL to 0x13 or 0'00010011
+	I2C2->TIMINGR &= ~(1 << 7);
+	I2C2->TIMINGR &= ~(1 << 6);
+	I2C2->TIMINGR &= ~(1 << 5);
+	I2C2->TIMINGR |= (1 << 4);
+	I2C2->TIMINGR &= ~(1 << 3);
+	I2C2->TIMINGR &= ~(1 << 2);
+	I2C2->TIMINGR |= (1 << 1);
+	I2C2->TIMINGR |= (1 << 0);
+	
+	// PE
+	I2C2->CR1 |= (1 << 0);
+	
+	/* I2C initialization
+	1. Set the slave address in the SADD[7:1] bit field.
+	2. Set the number of data byte to be transmitted in the NBYTES[7:0] bit field.
+	3. Configure the RD_WRN to indicate a read/write operation.
+	4. Do not set the AUTOEND bit, this lab requires software start/stop operation.
+	5. Setting the START bit to begin the address frame
+	*/
+	// SET ADD10 to 0
+	I2C2->CR2 &= ~(1 << 11);
+	// SADD to 0x69 0'b1101001 7 bits
+	/*I2C2->CR2 |= (1 << 9); DONT CARES
+	I2C2->CR2 |= (1 << 8);*/
+	I2C2->CR2 |= (1 << 7); 
+	I2C2->CR2 |= (1 << 6);
+	I2C2->CR2 &= ~(1 << 5);
+	I2C2->CR2 |= (1 << 4);
+	I2C2->CR2 &= ~(1 << 3);
+	I2C2->CR2 &= ~(1 << 2);
+	I2C2->CR2 |= (1 << 1);
+	/* I2C2->CR2 |= (1 << 0); DONT CARE */
+	// NBYTES to 0x1 or 0'b1
+	I2C2->CR2 &= ~(1 << 23);
+	I2C2->CR2 &= ~(1 << 22);
+	I2C2->CR2 &= ~(1 << 21);
+	I2C2->CR2 &= ~(1 << 20);
+	I2C2->CR2 &= ~(1 << 19);
+	I2C2->CR2 &= ~(1 << 18);
+	I2C2->CR2 &= ~(1 << 17);
+	I2C2->CR2 |= (1 << 16);
+	// RD_WRN to write
+	I2C2->CR2 &= ~(1 << 10);
+	// START
+	I2C2->CR2 |= (1 << 13);
+	
+	int doItOnce = 0;
+	
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+		if (doItOnce == 1) continue;
+		
+		// wait until TXIS(1) is set NACKF(4)
+		while ( !(I2C2->ISR & 2) ) {
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+			if (I2C2->ISR & 8) {	// error if this is true
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET); // turn on red led
+				doItOnce = 1;
+				continue;
+			}
+			HAL_Delay(50);
+		}
+		
+		if ((I2C2->ISR & 2) && !(I2C2->ISR & 8)) {
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET); // turn on red led
+			// set register of WHO_AM_I to TXDR which is 0x0F or 0'b00001111
+			I2C2->TXDR &= ~(1 << 7);
+			I2C2->TXDR &= ~(1 << 6);
+			I2C2->TXDR &= ~(1 << 5);
+			I2C2->TXDR &= ~(1 << 4);
+			I2C2->TXDR |= (1 << 3);
+			I2C2->TXDR |= (1 << 2);
+			I2C2->TXDR |= (1 << 1);
+			I2C2->TXDR |= (1 << 0);
+		}
+		
+		// wait until TC is set
+		while ( !(I2C2->ISR & 64) ) {
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);	// toggle blue LED
+			HAL_Delay(50);
+		}
+		
+		if (I2C2->ISR & 64) {
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET); // turn on blue led
+			// reload CR2 but set to read
+			// SET ADD10 to 0
+			I2C2->CR2 &= ~(1 << 11);
+			// SADD to 0x69 0'b1101001 7 bits
+			/*I2C2->CR2 |= (1 << 9); DONT CARES
+			I2C2->CR2 |= (1 << 8);*/
+			I2C2->CR2 |= (1 << 7); 
+			I2C2->CR2 |= (1 << 6);
+			I2C2->CR2 &= ~(1 << 5);
+			I2C2->CR2 |= (1 << 4);
+			I2C2->CR2 &= ~(1 << 3);
+			I2C2->CR2 &= ~(1 << 2);
+			I2C2->CR2 |= (1 << 1);
+			/* I2C2->CR2 |= (1 << 0); DONT CARE */
+			// NBYTES to 0x1 or 0'b1
+			I2C2->CR2 &= ~(1 << 23);
+			I2C2->CR2 &= ~(1 << 22);
+			I2C2->CR2 &= ~(1 << 21);
+			I2C2->CR2 &= ~(1 << 20);
+			I2C2->CR2 &= ~(1 << 19);
+			I2C2->CR2 &= ~(1 << 18);
+			I2C2->CR2 &= ~(1 << 17);
+			I2C2->CR2 |= (1 << 16);
+			// RD_WRN to read
+			I2C2->CR2 |= (1 << 10);
+			// START
+			I2C2->CR2 |= (1 << 13);
+		}
+		
+		// wait until RXNE is set
+		while ( !(I2C2->ISR & 4) ) {
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);	// toggle green LED
+			if (I2C2->ISR & 8) {	// error if this is true
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET); // turn on red led
+				doItOnce = 1;
+				continue;
+			}
+			HAL_Delay(50);
+		}
+		
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // turn on green led
+		
+		// wait until TC is set
+		while ( !(I2C2->ISR & 64) ) {
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);	// toggle orange LED
+			HAL_Delay(50);
+		}
+		
+		// check if RXDR matches with 0xD3 or 211
+		if (I2C2->RXDR == 211) {
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // turn on orange led
+			I2C2->CR2 |= (1 << 14);	// STOP I2C2
+			doItOnce = 1;
+		}
+			
   }
-  /* USER CODE END 3 */
+
 }
 
 /**
